503d7dc4e33d0f683f0b8224c7e39dc422570b6a
FabioTizV <60145360+FabioTizV@users.noreply.github.com>
2020-02-28 12:23:46 -0600

    Update Profesor.java

--- Encapsulamiento/clasesyobjetos/Profesor.java
+++ Encapsulamiento/clasesyobjetos/Profesor.java
@@ -8,6 +8,48 @@ public class Profesor
     private int matricula;
     private String escuela;
 
+    public void setNombre(String nombre) 
+    {
+
+        this.nombre = nombre;
+
+    }
+
+    public String getNombre ()
+    {
+
+        return this.nombre;
+
+    }
+
+    public void setMatricula (int matricula)
+    {
+
+        this.matricula = matricula;
+
+    }
+
+    public double getMatricula ()
+    {
+
+        return this.matricula;
+
+    }
+
+    public void setEscuela (String escuela)
+    {
+
+        this.escuela = escuela;
+
+    }
+
+    public String getEscuela()
+    {
+
+        return this.escuela;
+
+    }
+
     public Profesor()
     {
 
