4b7df44ab8130c5f1944e996452540fc85b6a631
FabioTizV <60145360+FabioTizV@users.noreply.github.com>
2020-02-28 12:05:14 -0600

    Update Animal.java

--- Encapsulamiento/clasesyobjetos/Animal.java
+++ Encapsulamiento/clasesyobjetos/Animal.java
@@ -7,6 +7,48 @@ public class Animal
     public String genero;
     public int edad;
     
+    public void setEspecie(String especie) 
+    {
+
+        this.especie = especie;
+
+    }
+
+    public String getEspecie ()
+    {
+
+        return this.especie;
+
+    }
+
+    public void setGenero (String genero)
+    {
+
+        this.genero = genero;
+
+    }
+
+    public String getGenero ()
+    {
+
+        return this.genero;
+
+    }
+
+    public void setEdad (int edad)
+    {
+
+        this.edad = edad;
+
+    }
+
+    public int getEdad()
+    {
+
+        return this.edad;
+
+    }
+    
     public Animal ()
     {
 
