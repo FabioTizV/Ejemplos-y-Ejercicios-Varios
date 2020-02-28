d26a4f4dc6546cb06ea0ff6ecc29add204991f05
FabioTizV <60145360+FabioTizV@users.noreply.github.com>
2020-02-28 12:04:07 -0600

    Update Alumno.java

--- Encapsulamiento/clasesyobjetos/Alumno.java
+++ Encapsulamiento/clasesyobjetos/Alumno.java
@@ -6,10 +6,45 @@ public class Alumno
     private int matricula;
     private String escuela;
 
+    public void setNombre(String nombre) 
+    {
+
+        this.nombre = nombre;
+
+    }
+
     public String getNombre ()
     {
 
         return this.nombre;
+
+    }
+
+    public void setMatricula (int matricula)
+    {
+
+        this.matricula = matricula;
+
+    }
+
+    public double getMatricula ()
+    {
+
+        return this.matricula;
+
+    }
+
+    public void setEscuela (String escuela)
+    {
+
+        this.escuela = escuela;
+
+    }
+
+    public String getEscuela()
+    {
+
+        return this.escuela;
 
     }
 
