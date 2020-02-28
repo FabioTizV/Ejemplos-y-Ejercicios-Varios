45e024e7baeb0793bb0e2d29e69e0b97187f2301
FabioTizV <60145360+FabioTizV@users.noreply.github.com>
2020-02-28 12:21:39 -0600

    Update Automovil.java

--- Encapsulamiento/clasesyobjetos/Automovil.java
+++ Encapsulamiento/clasesyobjetos/Automovil.java
@@ -7,6 +7,48 @@ public class Automovil
     public String modelo;
     public int año;
 
+    public void setMarca(String marca)
+    {
+
+        this.marca = marca;
+
+    }
+
+    public String getMarca ()
+    {
+
+        return this.marca;
+
+    }
+
+    public void setModelo (String modelo)
+    {
+
+        this.modelo = modelo;
+
+    }
+
+    public String getModelo ()
+    {
+
+        return this.modelo;
+
+    }
+
+    public void setAño (int año)
+    {
+
+        this.año = año;
+
+    }
+
+    public int getAño ()
+    {
+
+        return this.año;
+
+    }
+
     public Automovil ()
     {
 
